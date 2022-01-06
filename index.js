2cae5e5c4542b32131b633b6e8f4a2285d5f5e25
AricMcDonald <aricmcdonald@gmail.com>
2022-01-06 16:57:43 -0700

    consts

---
+++ index.js
@@ -0,0 +1,13 @@
+// TODO: Include packages needed for this application
+const fs = require('fs');
+const path = require('path');
+const insquirer = require('inquirer'); 
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown')
+// TODO: Create an array of questions for user input
+
+// TODO: Create a function to write README file
+
+// TODO: Create a function to initialize app
+
+// Function call to initialize app
